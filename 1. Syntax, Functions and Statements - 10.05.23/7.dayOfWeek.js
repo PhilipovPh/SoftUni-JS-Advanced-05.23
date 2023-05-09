d077780a4c02e7d687db01316d6f230e0610ca2b
PhilipovPh <philipov.ph@gmail.com>
2023-05-09 12:57:06 +0300

    s

--- 1. Syntax, Functions and Statements - 10.05.23/7.dayOfWeek.js
+++ 1. Syntax, Functions and Statements - 10.05.23/7.dayOfWeek.js
@@ -1,8 +1,30 @@
 function dayOfWeek(day) {
-    if (day > 0 && day < 8) {
-        switch (day) {
-            case "1":
-                break
-        }
+
+    switch (day) {
+        case 'Monday':
+            console.log(1);
+            break;
+        case 'Tuesday':
+            console.log(2);
+            break;
+        case 'Wednesday':
+            console.log(3);
+            break;
+        case 'Thurday':
+            console.log(4);
+            break;
+        case 'Friday':
+            console.log(5);
+            break;
+        case 'Saturday':
+            console.log(6);
+            break;
+        case 'Sunday':
+            console.log(7);
+            break;
+        case 'Invalid':
+            console.log('error');
     }
 }
+
+dayOfWeek('Monday')
