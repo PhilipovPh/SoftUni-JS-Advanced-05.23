41d9e88154845bc7430237b1e566f87116d56066
PhilipovPh <philipov.ph@gmail.com>
2023-05-22 19:49:07 +0300

    s

--- 6.Objects and Composition - 18.05.23/6.storeCatalogue.js
+++ 6.Objects and Composition - 18.05.23/6.storeCatalogue.js
@@ -1,5 +1,34 @@
 function storeCatalogue(input) {
+    const sortedInput = input.sort((a, b) => a.localeCompare(b));
+
+    const dict = {};
+
+    for (let i = 0; i < sortedInput.length; i++) {
+        const element = sortedInput[i].split(' : ').join(': ');
+        const firstLetter = element[0].toUpperCase();
+
+        if (!dict[firstLetter]) {
+            dict[firstLetter] = [];
+        }
+        dict[firstLetter].push(`  ${element}`);
+    }
+
+    let output = [];
+
+    for (const key in dict) {
+        const element = dict[key];
+        output = output.concat(key, element);
+    }
+    
+    return output.join('\n');
 
 }
 
-storeCatalogue();
+storeCatalogue(['Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']);
