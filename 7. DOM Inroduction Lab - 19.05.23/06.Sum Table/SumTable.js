48ca9761e2e8123fae0afc08a82c95d3a9854412
PhilipovPh <philipov.ph@gmail.com>
2023-05-29 15:09:41 +0300

    s

--- 7. DOM Inroduction Lab - 19.05.23/06.Sum Table/SumTable.js
+++ 7. DOM Inroduction Lab - 19.05.23/06.Sum Table/SumTable.js
@@ -1,3 +1,17 @@
 function sumTable() {
+    let tableElements = document.getElementsByTagName('tr');
 
+    let tableArr = Array.from(tableElements);
+    tableArr.shift();
+    tableArr.pop();
+
+    let sum = 0;
+
+    for (const line of tableArr) {
+        let price = Number(line.children[1].textContent);
+        sum += price;
+    }
+
+    let totalElement = document.getElementById('sum');
+    totalElement.textContent = sum;
 }
