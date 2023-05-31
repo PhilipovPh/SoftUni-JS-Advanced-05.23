e55a755f9be10d026dacd2b8043d4a0e0ad44c6a
PhilipovPh <philipov.ph@gmail.com>
2023-05-31 13:11:55 +0300

    s

--- 8. DOM Introduction Exercise - 22.05.23/03. Accordion/accordion.js
+++ 8. DOM Introduction Exercise - 22.05.23/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    console.log('TODO:...');
+
+    const MORE = 'More';
+    const LESS = 'Less';
+
+    let changeElement = document.querySelector('.button');
+    let lettextElement = document.querySelector('#extra');
+
+    changeElement.textContent = changeElement.textContent === MORE ? LESS : MORE;
+
+    lettextElement.style.display =
+        lettextElement.style.display === 'none' ||
+        lettextElement.style.display === '' ? 
+            'block' : 
+            'none';
 }
--- 8. DOM Introduction Exercise - 22.05.23/04. Search in List/search.js
+++ 8. DOM Introduction Exercise - 22.05.23/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+
+   let listItems = document.querySelectorAll('#towns li');
+   let inputItem = document.querySelector('#searchText').value;
+   let result = document.querySelector('#result');
+
+   let sum = 0
+
+   for (const li of listItems) {
+      let liToLowerCase = li.textContent.toLowerCase();
+      let inputToLowerCase = inputItem.toLowerCase();
+
+      if (liToLowerCase.includes(inputToLowerCase)) {
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+         sum++;
+      } else {
+         li.style.fontWeight = '';
+         li.style.textDecoration = '';
+      }
+   }
+
+   result.textContent = `${sum} matches found`;
 }
