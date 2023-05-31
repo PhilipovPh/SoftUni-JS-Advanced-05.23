ac84a7aaea6581f922aecf21173c08efffac1c42
PhilipovPh <philipov.ph@gmail.com>
2023-05-31 14:46:04 +0300

    s

--- 8. DOM Introduction Exercise - 22.05.23/06. Format the Text/solution.js
+++ 8. DOM Introduction Exercise - 22.05.23/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  let textAriaValue = document.getElementById('input').value;
+  let resultElement = document.getElementById('output');
+
+  let textArr = textAriaValue
+    .split('.')
+    .filter(s => s !== '')
+    .map(s => s + '.');
+
+  const paragraphRoof = Math.ceil(textArr.length / 3);
+
+  for (let i = 0; i < paragraphRoof; i++) {
+    const joinedSentences = textArr.splice(0, 3).join('');
+
+    resultElement.innerHTML += `<p>${joinedSentences}</p>`
+
+  }
 }
