ff7c70265d248ae030920655cfa8837f5c0e4ab9
PhilipovPh <philipov.ph@gmail.com>
2023-06-02 20:33:07 +0300

    s

--- 9.DOM Events Lab - 23.05.23/03. Add-Delete/app.js
+++ 9.DOM Events Lab - 23.05.23/03. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    let itemElements = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let newElement = document.createElement('li');
+    newElement.textContent = inputElement.value;
+    inputElement.value = '';
+
+    let deleteButton = document.createElement('a');
+    deleteButton.href = '#';
+    deleteButton.textContent = '[Delete]';
+    deleteButton.addEventListener('click', (e) => {
+        e.currentTarget.parentElement.remove();
+    });
+
+    newElement.appendChild(deleteButton);
+    itemElements.appendChild(newElement);
 }
--- 9.DOM Events Lab - 23.05.23/05. Highlight-Active/app.js
+++ 9.DOM Events Lab - 23.05.23/05. Highlight-Active/app.js
@@ -1,3 +1,21 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElement = document.querySelectorAll('input');
+    let inputArr = Array.from(inputElement);
+
+    inputArr.forEach(x => {
+        x.addEventListener('focus', focusHendler);
+        x.addEventListener('blur', blurHendler);
+    })
+
+    function focusHendler(e) {
+        let element = e.target;
+        let div = element.parentElement;
+        div.classList.add('focused');
+    }
+
+    function blurHendler(e) {
+        let element = e.target;
+        let div = element.parentElement;
+        div.classList.remove('focused')
+    }
 }
