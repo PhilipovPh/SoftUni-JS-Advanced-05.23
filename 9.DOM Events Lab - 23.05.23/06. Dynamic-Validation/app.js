4564bda0ba63003a57683465b6ed8b851558e533
PhilipovPh <philipov.ph@gmail.com>
2023-06-02 22:16:57 +0300

    s

--- 9.DOM Events Lab - 23.05.23/06. Dynamic-Validation/app.js
+++ 9.DOM Events Lab - 23.05.23/06. Dynamic-Validation/app.js
@@ -1,3 +1,24 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById('email')
+    input.addEventListener('change', changeHandler)
+ 
+ 
+    let regex = /^[a-z]*@[a-z]*.[a-z]*$/g
+ 
+    function changeHandler(e) {
+        let element = e.target
+        let value = element.value
+ 
+        let test = regex.test(value)
+        if (test) {
+            input.classList.remove('error')
+ 
+        } else {
+            input.classList.add('error')
+ 
+        }
+ 
+ 
+    }
+ 
 }
--- 9.DOM Events Lab - 23.05.23/07. Shopping-Cart/solution.js
+++ 9.DOM Events Lab - 23.05.23/07. Shopping-Cart/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-   //TODO...
+   let addButtons = document.querySelectorAll('.add-product');
+   let addButtonsArr = Array.from(addButtons);
+   let textArea = document.querySelector('textarea');
+   let totalAum = 0;
+   let checkoutBtn = document.querySelector('.checkout');
+
+   addButtonsArr.forEach(x => x.addEventListener('click', addHandler));
+   checkoutBtn.addEventListener('click', checkoutHandler)
+
+   function checkoutHandler(e) {
+      addButtonsArr.forEach(x => x.dsabled = 'disabled');
+      let productNames = Object.keys(products).join(', ')
+   }
+
+   function addHandler(e) {
+      let element = e.target;
+      let productDiv = element.parentElement.parentElement;
+
+      let title = productDiv.querySelector('.product-title');
+      let priceDiv = productDiv.querySelector('.product-line-price');
+
+      let name = title.textContent;
+      let price = priceDiv.textContent;
+      totalAum += price;
+      products[name] = true;
+
+      textArea.textContent += `Aded ${pro} for ${price.toFixed(2)} to the cart \n`;
+   }
 }
