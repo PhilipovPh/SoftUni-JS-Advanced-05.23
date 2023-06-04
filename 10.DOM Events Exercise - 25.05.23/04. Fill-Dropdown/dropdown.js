e6cd27528ef507f76f8dc91dc126a559f1319a22
PhilipovPh <philipov.ph@gmail.com>
2023-06-04 21:38:01 +0300

    s

--- 10.DOM Events Exercise - 25.05.23/04. Fill-Dropdown/dropdown.js
+++ 10.DOM Events Exercise - 25.05.23/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputTextElement = document.getElementById('newItemText');
+    const inputValueElement = document.getElementById('newItemValue');
+    const menu = document.getElementById('menu');
+
+    const optionMenu = document.createElement('option');
+
+    optionMenu.textContent = inputTextElement.value;
+    optionMenu.value = inputValueElement.value;
+
+    menu.appendChild(optionMenu);
+
+    inputTextElement.value = "";
+    inputValueElement.value = "";
 }
