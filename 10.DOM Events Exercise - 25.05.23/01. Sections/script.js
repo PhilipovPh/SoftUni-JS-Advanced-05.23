457bbbbf3db68735a05f1b750c2d6790f026feb6
PhilipovPh <philipov.ph@gmail.com>
2023-06-04 19:05:19 +0300

    s

--- 10.DOM Events Exercise - 25.05.23/01. Sections/script.js
+++ 10.DOM Events Exercise - 25.05.23/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+   const contendDiv = document.getElementById('content');
+
+   words.forEach(word => {
+
+      const div = document.createElement('div');
+      const praragraph = document.createElement('p');
+
+      praragraph.textContent = word;
+      praragraph.style.display = 'none';
+
+      div.appendChild(praragraph);
+      div.addEventListener('click', revealTextonClick);
+
+      contendDiv.appendChild(div);
+   });
+
+   function revealTextonClick(e) {
+      e.currentTarget.children[0].style.display = 'block';
+   }
 }
