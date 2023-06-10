f410bffd4af4ac013f7a490881baf7b8d9a210d2
PhilipovPh <philipov.ph@gmail.com>
2023-06-10 09:44:23 +0300

    s

--- 11.Advanced functions Lab - 26.05.23/2.add.js
+++ 11.Advanced functions Lab - 26.05.23/2.add.js
@@ -1,10 +1,7 @@
 function solution(number) {
-    function add(a, b) {
-        return a + b;
+    return (num) => {
+        return number + num;
     }
-    
-    return add.bind(this, number);
-    // return (n) => add(number, n); 
 }
 
 let add5 = solution(5);
