d400e047a1d3623f6ee2bdac63dc39ff7b8822f1
PhilipovPh <philipov.ph@gmail.com>
2023-06-14 13:45:50 +0300

    s

---
+++ 17,Prototypes and Inheritance Lab - 06.06.23/1.person.js
@@ -0,0 +1,28 @@
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set fullName(value) {
+        const nameParts = value.split(' ');
+        if (nameParts.length === 2) {
+            this.firstName = nameParts[0];
+            this.lastName = nameParts[1];
+        }
+    }
+}
+
+let person = createPerson("Peter", "Ivanov");
+console.log(person.fullName); //Peter Ivanov
+person.firstName = "George";
+console.log(person.fullName); //George Ivanov
+person.lastName = "Peterson";
+console.log(person.fullName); //George Peterson
+person.fullName = "Nikola Tesla";
+console.log(person.firstName); //Nikola
+console.log(person.lastName); //Tesla
